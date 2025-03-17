b98b233fc03d266b1908508dc9d8e2f1c7d237ea
JuanPR <Juanpramirez@ucundinamarca.edu.co>
2025-03-16 21:20:42 -0500

    Se creo la clase empleado, la cual usaran para guardar todos los datos tales como el id, nombre, cargo y salario de cada empleado

--- src/GestionEmpleados.java
+++ src/GestionEmpleados.java
@@ -1,3 +1,43 @@
+// Clase Empleado
+class Empleado {
+    // Atributos
+    private int id;
+    private String nombre;
+    private String cargo;
+    private double salario;
+
+    // Constructor
+    public Empleado(int id, String nombre, String cargo, double salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    // Método para mostrar detalles del empleado
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nombre: " + nombre + ", Cargo: " + cargo + ", Salario: " + salario;
+    }
+}
+
 
 public class GestionEmpleados {
 
